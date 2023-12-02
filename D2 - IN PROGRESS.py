c698edd527e63b440cd705c4c029148a0c79b3a6
Yusuf Ihsan Orung <yusuforung@gmail.com>
2023-12-02 03:24:50 +0300

    D1 done

---
+++ D2 - IN PROGRESS.py
@@ -0,0 +1,21 @@
+calibrationInputs = open('CalibrationInputs.txt', 'r')
+calibrationLines = calibrationInputs.readlines()
+
+calibrationValue = 0
+
+for line in calibrationLines:
+    calibrationNumber = ""
+    firstChar = ''
+    secondChar = ''
+    for char in line:
+        if 58 > ord(char) > 47:
+            if firstChar == '':
+                firstChar = char
+            else:
+                secondChar = char
+    calibrationNumber = (firstChar + secondChar)
+    if len(calibrationNumber) == 1:
+        calibrationNumber = (calibrationNumber*2)
+    calibrationValue += int(calibrationNumber)
+
+print(calibrationValue)
